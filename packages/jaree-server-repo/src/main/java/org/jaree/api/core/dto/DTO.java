09afb05f76a1f207fae7bf5d24488cf74ce2a17d
Dante <148848550+s12171934@users.noreply.github.com>
2025-12-24 22:17:13 +0900

    DTO abstract class documentation

--- packages/jaree-server-repo/src/main/java/org/jaree/api/core/dto/DTO.java
+++ packages/jaree-server-repo/src/main/java/org/jaree/api/core/dto/DTO.java
@@ -1,5 +1,11 @@
 package org.jaree.api.core.dto;
 
+/**
+ * @category DTO
+ * @apiNote DTO에 RequiredEntityRecord를 제너릭으로 추가하기 위한 추상 클래스
+ * @version 1.0
+ * @since 2025-12-24
+ */
 public abstract class DTO<RequiredEntityRecord> {
 
 }
