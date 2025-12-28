a77b51ed52282fca8043e692c0cd31a8af8fa1aa
leesyseel <leesyseel@gmail.com>
2025-12-28 14:21:48 +0900

    refactor code format and rename variable

--- packages/jaree-server-repo/src/main/java/org/jaree/api/company/repository/CompanyRepository.java
+++ packages/jaree-server-repo/src/main/java/org/jaree/api/company/repository/CompanyRepository.java
@@ -8,13 +8,13 @@ import org.springframework.data.neo4j.repository.Neo4jRepository;
 
 public interface CompanyRepository extends Neo4jRepository<Company, Long> {
     @Query("""
-    MATCH (c:Company)
-    WHERE elementId(c) = $id
-    OPTIONAL MATCH (c)<-[r1:CREATED_BY]-(j:JobOpening)
-    OPTIONAL MATCH (j)-[r2:ASKS]->(q:ApplicationQuestion)
-    RETURN c,
+        MATCH (c:Company)
+        WHERE elementId(c) = $id
+        OPTIONAL MATCH (c)<-[r1:CREATED_BY]-(j:JobOpening)
+        OPTIONAL MATCH (j)-[r2:ASKS]->(q:ApplicationQuestion)
+        RETURN c,
         collect(r1) AS rel_j, collect(j) AS jobOpenings,
-        collect(r2) AS rel_q, collect(q) AS applicationQuestion
-    """)
+        collect(r2) AS rel_q, collect(q) AS questions
+        """)
     Optional<Company> findByIdWithJobOpeningsWithQuestion(Long id);
 }
