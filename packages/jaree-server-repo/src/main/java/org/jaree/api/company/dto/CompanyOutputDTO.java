6aef729565a8db163b28f24f2becfedf316c7149
leesyseel <leesyseel@gmail.com>
2025-12-28 14:54:21 +0900

    add temp entity-to-dto converting func

--- packages/jaree-server-repo/src/main/java/org/jaree/api/company/dto/CompanyOutputDTO.java
+++ packages/jaree-server-repo/src/main/java/org/jaree/api/company/dto/CompanyOutputDTO.java
@@ -1,11 +1,36 @@
 package org.jaree.api.company.dto;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
+import org.jaree.api.company.entity.Company;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class CompanyOutputDTO {
     Long id;
     String name;
     String description;
     String logoUrl;
     List<String> categories;
+
+    public static CompanyOutputDTO from(Company company) {
+
+        if(company == null) return null;
+
+        List<String> categories = company.getCategories() == null
+            ? new ArrayList<>()
+            : company.getCategories().stream()
+            .filter(Objects::nonNull)
+            .map(Enum::name)
+            .toList();
+
+        return new CompanyOutputDTO(
+            company.getId(), company.getName(),
+            company.getDescription(), company.getLogoUrl(),
+            categories
+        );
+    }
 }
--- packages/jaree-server-repo/src/main/java/org/jaree/api/jobopening/dto/JobOpeningOutputDTO.java
+++ packages/jaree-server-repo/src/main/java/org/jaree/api/jobopening/dto/JobOpeningOutputDTO.java
@@ -2,9 +2,15 @@ package org.jaree.api.jobopening.dto;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 
+import org.jaree.api.application.entity.ApplicationQuestion;
 import org.jaree.api.company.dto.CompanyOutputDTO;
+import org.jaree.api.jobopening.entity.JobOpening;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class JobOpeningOutputDTO{
     Long id;
     String title;
@@ -17,10 +23,41 @@ public class JobOpeningOutputDTO{
     CompanyOutputDTO company;
     List<ApplicationQuestionDTO> questions;
 
+    @AllArgsConstructor
     public static class ApplicationQuestionDTO{
         Long id;
         String content;
         String description;
         Integer order;
+
+        public static ApplicationQuestionDTO from(ApplicationQuestion question){
+            if(question == null) return null;
+
+            return new ApplicationQuestionDTO(
+                question.getId(), question.getContent(),
+                question.getDescription(), question.getOrder());
+        }
+    }
+
+    public static JobOpeningOutputDTO from(JobOpening jobOpening){
+
+        if(jobOpening == null) return null;
+
+        CompanyOutputDTO company = CompanyOutputDTO.from(jobOpening.getCompany());
+
+        List<ApplicationQuestionDTO> questions = jobOpening.getQuestions() == null
+            ? List.of()
+            : jobOpening.getQuestions().stream()
+                .filter(Objects::nonNull)
+                .map(ApplicationQuestionDTO::from)
+                .toList();
+
+        return new JobOpeningOutputDTO(
+            jobOpening.getId(), jobOpening.getTitle(),
+            jobOpening.getDescription(), jobOpening.getContentS3Url(),
+            jobOpening.getImageUrl(), jobOpening.getStartsAt(),
+            jobOpening.getEndsAt(), jobOpening.getCreatedAt(),
+            company, questions
+        );
     }
 }
