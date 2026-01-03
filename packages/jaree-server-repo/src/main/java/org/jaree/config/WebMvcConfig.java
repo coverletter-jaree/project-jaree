11858f9c7ea209d9c520eb77c0aeb9a923ef9272
Dante <148848550+s12171934@users.noreply.github.com>
2026-01-03 21:39:22 +0900

    apply gemini review

--- packages/jaree-server-repo/src/main/java/org/jaree/config/WebMvcConfig.java
+++ packages/jaree-server-repo/src/main/java/org/jaree/config/WebMvcConfig.java
@@ -1,22 +1,20 @@
 package org.jaree.config;
 
-import java.util.Objects;
-
 import org.springframework.context.annotation.Configuration;
-import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
-  private final String API_VERSION = "v1";
+    private static final String API_VERSION = "v1";
 
-  /**
-   * Global prefix for API versioning
-   */
-  @Override
-  public void configurePathMatch(@NonNull PathMatchConfigurer configurer) {
-    String apiPrefixWithVersion = String.format("/api/%s", API_VERSION);
-    configurer.addPathPrefix(Objects.requireNonNull(apiPrefixWithVersion), c -> true);
-  }
+    /**
+     * Global prefix for API versioning
+     */
+    @Override
+    @SuppressWarnings("null")
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        String apiPrefixWithVersion = String.format("/api/%s", API_VERSION);
+        configurer.addPathPrefix(apiPrefixWithVersion, c -> true);
+    }
 }
